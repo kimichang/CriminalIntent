b390e597c23b9d89bd14f078af490bf465f1ab23
kimichangsign@gmail.com <kimichang262263?>
2016-06-24 14:39:18 +0800

    Initial commit

---
+++ app/src/main/java/com/newgeniuser/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.newgeniuser.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by lenovo on 2016/6/24.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId=UUID.randomUUID();
+        mDate=new Date();
+    }
+
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
